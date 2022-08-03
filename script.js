bdc0286bfe302083eb331a10c7b9381eff85d1aa
Nick <Nick_II2000@mail.ru>
2022-08-03 20:55:48 +0600

    8-th commit

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 'use strict';
 // Ex. 106
 
-try {
-    console.log('Normal');
-    console.log(a);
-    console.log('Result');
-} catch (error) {
-    console.log('Error');
-    console.log('=======================');
-    console.log(error);
-    console.log(error.name);
-    console.log(error.message);
-    console.log(error.stack);
-    console.log('=======================');
-} finally {
-    console.log('Final');
-}
+// try {
+//     console.log('Normal');
+//     console.log(a);
+//     console.log('Result');
+// } catch (error) {
+//     console.log('Error');
+//     console.log('=======================');
+//     console.log(error);
+//     console.log(error.name);
+//     console.log(error.message);
+//     console.log(error.stack);
+//     console.log('=======================');
+// } finally {
+//     console.log('Final');
+// }
 
-console.log('Still normal');
-console.log('\n!!!!!!!!!!!!!!!!!!!!!!!');
+// console.log('Still normal');
+// console.log('\n!!!!!!!!!!!!!!!!!!!!!!!');
+
+document.querySelector('.active').addEventListener('click', () => {
+    console.log('click');
+});
+
+console.log('Normal');
