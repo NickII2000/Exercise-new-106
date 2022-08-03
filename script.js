fabf639a43a34057934b57516e9ce068369bcd47
Nick <Nick_II2000@mail.ru>
2022-08-03 20:32:04 +0600

    3-th commit

--- script.js
+++ script.js
@@ -3,8 +3,12 @@
 
 try {
     console.log('Normal');
+    console.log(a);
+    console.log('Result');
 } catch (e) {
     console.log('Error');
 } finally {
     console.log('Final');
 }
+
+console.log('Still normal');
