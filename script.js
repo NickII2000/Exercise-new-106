482ed67701c421206cc60a1e17ef10c4069edff4
Nick <Nick_II2000@mail.ru>
2022-08-03 20:33:36 +0600

    4-th commit

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ try {
     console.log('Result');
 } catch (e) {
     console.log('Error');
+    console.log('=======================');
+    console.log(e);
+    console.log('=======================');
 } finally {
     console.log('Final');
 }
